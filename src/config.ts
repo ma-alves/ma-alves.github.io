ca1053cea62bc545e5b411d7f0048397236af1a5
matheus <matheusamenos@gmail.com>
2025-12-17 19:16:09 -0300

    Faz ajustes em tsconfig para personalização

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,17 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
-  accentColor: "#1d4ed8",
+  name: "Matheus Alves",
+  title: "Back-End Developer",
+  description: "Portfolio website of Matheus Alves",
+  accentColor: "#e02b2bff",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "matheusvialves@outlook.com",
+    linkedin: "https://linkedin.com/in/matheusvialves",
+    // twitter: "https://x.com/rfitzio",
+    github: "https://github.com/ma-alves",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
-  skills: ["Javascript", "React", "Node.js", "Python", "AWS", "Docker"],
+  skills: ["Python", "Django", "FastAPI", "PostgreSQL", "Docker"],
   projects: [
     {
       name: "AI Dev Roundup Newsletter",
